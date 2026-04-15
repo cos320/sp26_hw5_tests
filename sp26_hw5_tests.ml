884ad4c1d89153791a89509b737ca57a1892a5c2
Zachary Kincaid <zkincaid@gmail.com>
2026-04-14 21:18:13 -0400

    Initial check-in

---
+++ sp26_hw5_tests.ml
@@ -0,0 +1,45 @@
+(** See the project instructions for more details about test case requirements.
+
+    Add your test case to this list.
+
+    Each test case is a commented triple of the form:
+
+    (* TEAM: Steve Zdancewic and Awesome T. Assistant
+       Description: <description>
+    *)
+    (<filename>,<stdin>,<expected_stdout>)
+
+    - include the names of your team members in a comment above the test
+      along with a brief <description> of what Oat features are exercised
+    
+    - <filename> should name an *.oat file that appears in this directory
+      It should should declare the standard oat entry point:
+
+      int program(int argc, string[] argv)
+
+    - <stdin> should be the string passed as the command-line arguments
+      to the executable generaged by compiling <filename> with
+      the following sequence of commands:
+
+      ./oatc <filename> bin/runtime.c
+      ./a.out <stdin>
+
+    - <expected_stdout> is the string representing the expected result
+      obtained by running the compiled <filename> on <stdin> and then
+      concatenating the status code (0-255) returned by the call.
+
+    You can use the command line to compile and run such tests like this:
+
+    > ./oatc sp26_hw5_tests/regalloctest3.oat bin/runtime.c
+    > ./a.out
+    > echo $?
+    0
+
+    These test cases will be run twice:
+     - once for "correctness" using the pass_all_executed_oat_file test harness
+     - once for "quality" using the quality_oat test harness
+     See test/gradedtest.ml for more details and examples
+*)
+let student_tests : (string * string * string) list = [
+    ("regalloctest3.oat", "", "0");
+] 
